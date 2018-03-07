ebfbd753fa5f650ed980decf2fd882750adaa5bc
Michael Pope <popemray1986@gmail.com>
2018-03-07 04:44:38 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,8 +7,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       cs.integer :costume_inventory
       cs.integer :num_of_employees
       cs.boolean :still_in_business
-      cs.start_time :opening_time
-      cs.end_time :closing_time
+      cs.datetime :opening_time
+      cs.datetime :closing_time
     end
   end
 
